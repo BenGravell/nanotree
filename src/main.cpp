a02778718a762a9750129ee2f8a10f386d90da17
Ben Gravell <bjgravell@gmail.com>
2025-08-02 10:48:21 +0200

    add num nodes select

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,22 @@
 
 int main() {
     struct Node { Vector2 pos; std::shared_ptr<Node> parent; };
-    InitWindow(800, 800, "nanotree");
+    InitWindow(800, 900, "nanotree");
     Vector2 goal = {750, 400};
     std::vector<Vector2> obstacles = {{400, 400}};
     std::vector<std::shared_ptr<Node>> nodes;
     std::vector<std::shared_ptr<Node>> path;
+    int num_nodes = 1000;
     while (!WindowShouldClose()) {
         Vector2 mouse = {std::clamp(GetMousePosition().x, 0.0f, 800.0f), std::clamp(GetMousePosition().y, 0.0f, 800.0f)};
         if (IsMouseButtonDown(0)) goal = mouse;
         if (IsMouseButtonDown(1) && !std::ranges::any_of(obstacles, [&](auto& o) { return Vector2Distance(o, mouse) < 5; })) obstacles.push_back(mouse);
         if (IsMouseButtonDown(2)) obstacles.erase(std::remove_if(obstacles.begin(), obstacles.end(), [&]( Vector2 o) { return Vector2Distance(o, mouse) < 50; }), obstacles.end());
+        num_nodes = std::clamp(num_nodes + ((GetMouseWheelMove() > 0) - (GetMouseWheelMove() < 0)) * 100, 0, 5000);
         if (IsMouseButtonDown(0) || IsMouseButtonDown(1) || IsMouseButtonDown(2) || nodes.empty()) {
             nodes = {std::make_shared<Node>(Node{{50, 400}, nullptr})};
-            for (int i = 0; i <= 1000; ++i) {
-                Vector2 pos = (i == 1000) ? goal : Vector2(800 * float(std::rand()) / RAND_MAX, 800 * float(std::rand()) / RAND_MAX);
+            for (int i = 0; i <= num_nodes; ++i) {
+                Vector2 pos = (i == num_nodes) ? goal : Vector2(800 * float(std::rand()) / RAND_MAX, 800 * float(std::rand()) / RAND_MAX);
                 std::shared_ptr<Node> parent = *std::min_element(nodes.begin(), nodes.end(), [&pos]( std::shared_ptr<Node>& a,  std::shared_ptr<Node>& b) { return Vector2Distance(a->pos, pos) < Vector2Distance(b->pos, pos); });
                 pos = Vector2Add(parent->pos, Vector2Scale(Vector2Subtract(pos, parent->pos), std::min(1.0f, 20.0f / Vector2Distance(parent->pos, pos))));
                 if (!std::any_of(obstacles.begin(), obstacles.end(), [&pos](auto& obs) { return Vector2Distance(obs, pos) < 50; })) nodes.push_back(std::make_shared<Node>(pos, parent));
@@ -34,14 +36,15 @@ int main() {
             std::reverse(path.begin(), path.end());
         }
         BeginDrawing();
-        DrawRectangle(0, 0, 800, 800, Fade(BLACK, 0.1f));
+        DrawRectangle(0, 0, 800, 900, Fade(BLACK, 0.1f));
         for (auto obstacle : obstacles) DrawCircleV(obstacle, 50, DARKGRAY);
         for (auto node : nodes) if (node->parent) DrawLineEx(node->parent->pos, node->pos, 2, BLUE);
         for (auto node : path) DrawLineEx(node->parent->pos, node->pos, 8, RAYWHITE);
         DrawRectangle(goal.x - 12, goal.y - 12, 24, 24, GOLD);
-        DrawRectangle(0, 0, 800, 40, {40, 40, 40, 255});
-        DrawText("LMB: move goal    RMB: add obstacle    MMB: remove obstacle", 140, 10, 20, RAYWHITE);
-        DrawFPS(10, 10);
+        DrawRectangle(0, 800, 800, 100, {40, 40, 40, 255});
+        DrawText("LMB: move goal  RMB: add obstacle  MMB: del obstacle  Scroll: max nodes", 10, 840, 20, RAYWHITE);
+        DrawText((std::to_string(num_nodes) + " max nodes").c_str(), 140, 810, 20, BLUE);
+        DrawFPS(10, 810);
         EndDrawing();
     }
     CloseWindow();
