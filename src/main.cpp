bbaa4c515f6d54201b521a86adce3f6bf0f751bb
Ben Gravell <bjgravell@gmail.com>
2025-08-07 20:49:42 +0200

    refactor and chooseParent upgrade

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,6 @@
 
 #include "roma_r.h"
 
-inline Color mapToColor(const float x, const std::array<std::array<uint8_t, 3>, 256>& colormap) {
-    const int idx = std::clamp(static_cast<int>(x * 255.0f + 0.5f), 0, 255);
-    const auto& rgb = colormap[idx];
-    return Color{rgb[0], rgb[1], rgb[2], 255};
-}
-
 std::mt19937 rng(std::random_device{}());
 
 static constexpr int ENVIRONMENT_WIDTH = 2000;
@@ -35,6 +29,16 @@ static constexpr Color NODE_COUNT_COLOR = PURPLE;
 static constexpr int OBSTACLE_RADIUS = 100;
 static constexpr float DEVIATION_DISTANCE_MAX = 0.8f * OBSTACLE_RADIUS;
 static constexpr float RADIUS_OF_CURVATURE_MIN = 1.1f * OBSTACLE_RADIUS;
+
+static constexpr int MOUSE_CENTER_RADIUS = 10;
+static constexpr int OBSTACLE_SPACING_MIN = 10;
+
+static constexpr int LINE_WIDTH_PATH = 12;
+static constexpr int LINE_WIDTH_TREE = 3;
+static constexpr int NODE_RADIUS_PATH = 1.5 * LINE_WIDTH_PATH;
+
+static constexpr float CHEAP_PARENT_SEARCH_RADIUS = 0.8f * DEVIATION_DISTANCE_MAX;
+static constexpr float GOAL_REACHED_RADIUS = 40.0f;
 
 // TODO make static constexpr std::array
 const std::vector<int> samples_options = {0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000};
@@ -79,6 +83,65 @@ Vector2 attractByAngle(const Vector2 pos, const std::shared_ptr<Node> parent) {
     return Vector2Add(parent->pos, direction_out * distance_yz);
 }
 
+std::shared_ptr<Node> chooseParent(const Vector2 pos, const std::vector<std::shared_ptr<Node>>& nodes) {
+    auto cheapest_it = std::min_element(nodes.begin(), nodes.end(),
+                                        [&pos](const std::shared_ptr<Node>& a, const std::shared_ptr<Node>& b) {
+                                            float da = Vector2Distance(a->pos, pos);
+                                            float db = Vector2Distance(b->pos, pos);
+                                            bool a_valid = da < CHEAP_PARENT_SEARCH_RADIUS;
+                                            bool b_valid = db < CHEAP_PARENT_SEARCH_RADIUS;
+
+                                            if (a_valid && b_valid)
+                                                return (a->cost_to_come + da) < (b->cost_to_come + db);
+                                            if (a_valid)
+                                                return true;
+                                            if (b_valid)
+                                                return false;
+                                            return false;
+                                        });
+
+    if (Vector2Distance((*cheapest_it)->pos, pos) < CHEAP_PARENT_SEARCH_RADIUS) {
+        return *cheapest_it;
+    }
+
+    return *std::min_element(nodes.begin(), nodes.end(),
+                             [&pos](const std::shared_ptr<Node>& a, const std::shared_ptr<Node>& b) {
+                                 return Vector2Distance(a->pos, pos) < Vector2Distance(b->pos, pos);
+                             });
+}
+
+inline Color fpsColor(const int fps) {
+    if (fps < 15) return RED;
+    if (fps < 30) return GOLD;
+    return BLUE;
+}
+
+inline Color mapToColor(const float x, const std::array<std::array<uint8_t, 3>, 256>& colormap) {
+    const int idx = std::clamp(static_cast<int>(x * 255.0f + 0.5f), 0, 255);
+    const auto& rgb = colormap[idx];
+    return Color{rgb[0], rgb[1], rgb[2], 255};
+}
+
+void DrawSelector(const Vector2 pos) {
+    static constexpr float selector_orbit_period = 5.0f;
+    static constexpr int num_segments = 8;
+    static constexpr int delta_angle = 360 / (2 * num_segments);
+    static constexpr int ring_outer_radius = OBSTACLE_RADIUS;
+    static constexpr int ring_width = 0.1 * OBSTACLE_RADIUS;
+
+    const float current_time = GetTime();
+    const int offset_angle = fmodf(current_time / selector_orbit_period, 1.0f) * 360;
+    const int offset_angle2 = fmodf(-current_time / selector_orbit_period, 1.0f) * 360;
+    DrawCircleV(pos, OBSTACLE_RADIUS, Fade(GRAY, 0.4f));
+    DrawRing(pos, ring_outer_radius - ring_width, ring_outer_radius, 0, 360, 0, Fade(GRAY, 0.6f));
+    for (int i = 0; i < num_segments; ++i) {
+        const int start_angle = 2 * i * delta_angle + offset_angle;
+        const int start_angle2 = 2 * i * delta_angle + offset_angle2;
+        DrawRing(pos, ring_outer_radius - ring_width / 2, ring_outer_radius, start_angle, start_angle + delta_angle, 0, LIGHTGRAY);
+        DrawRing(pos, ring_outer_radius - ring_width, ring_outer_radius - ring_width / 2, start_angle2, start_angle2 + delta_angle, 0, LIGHTGRAY);
+    }
+}
+
 int main() {
     SetTraceLogLevel(LOG_ERROR);
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "nanotree");
@@ -107,7 +170,7 @@ int main() {
             goal = mouse;
         }
 
-        if (is_down_rmb && std::none_of(obstacles.begin(), obstacles.end(), [&](auto& o) { return Vector2Distance(o, mouse) < 10; })) {
+        if (is_down_rmb && std::none_of(obstacles.begin(), obstacles.end(), [&](auto& o) { return Vector2Distance(o, mouse) < OBSTACLE_SPACING_MIN; })) {
             obstacles.push_back(mouse);
         }
 
@@ -122,7 +185,7 @@ int main() {
             for (int i = 0; i <= samples; ++i) {
                 Vector2 pos = (i == samples) ? goal : sample();
 
-                std::shared_ptr<Node> parent = *std::min_element(nodes.begin(), nodes.end(), [&pos](std::shared_ptr<Node>& a, std::shared_ptr<Node>& b) { return Vector2Distance(a->pos, pos) < Vector2Distance(b->pos, pos); });
+                std::shared_ptr<Node> parent = chooseParent(pos, nodes);
 
                 pos = clampToEnvironment(pos);
                 pos = attractByDistance(pos, parent);
@@ -149,13 +212,14 @@ int main() {
             std::reverse(path.begin(), path.end());
         }
 
-        // TODO 40 to goal tolerance
-        bool solved = Vector2Distance(path.empty() ? nodes.front()->pos : path.back()->pos, goal) < 40;
+        const bool solved = Vector2Distance(path.empty() ? nodes.front()->pos : path.back()->pos, goal) < GOAL_REACHED_RADIUS;
 
         // DRAWING SECTION
         BeginDrawing();
         DrawRectangle(0, 0, ENVIRONMENT_WIDTH, ENVIRONMENT_HEIGHT, BLACK);
-        for (auto obstacle : obstacles) DrawCircleV(obstacle, OBSTACLE_RADIUS, DARKGRAY);
+        for (auto obstacle : obstacles) {
+            DrawCircleV(obstacle, OBSTACLE_RADIUS, DARKGRAY);
+        }
 
         float cost_to_come_goal = 1.0f;
         float cost_to_come_max = 2.0f * cost_to_come_goal;
@@ -166,6 +230,7 @@ int main() {
             cost_to_come_max = std::max(cost_to_come_max, node->cost_to_come);
         }
 
+        // TODO func DrawTree
         for (auto node : nodes) {
             if (!node->parent) {
                 continue;
@@ -179,34 +244,17 @@ int main() {
             }
             c = std::clamp(c, 0.0f, 1.0f);
             const Color color = mapToColor(c, roma_r_colormap);
-            DrawLineEx(node->parent->pos, node->pos, 2, color);
+            DrawLineEx(node->parent->pos, node->pos, LINE_WIDTH_TREE, color);
         }
 
+        // TODO func DrawPath
         for (auto node : path) {
-            DrawLineEx(node->parent->pos, node->pos, 10, RAYWHITE);
-            DrawCircleV(node->pos, 10, RAYWHITE);
+            DrawLineEx(node->parent->pos, node->pos, LINE_WIDTH_PATH, RAYWHITE);
+            DrawCircleV(node->pos, NODE_RADIUS_PATH, RAYWHITE);
         }
 
-        const float current_time = GetTime();
-
-        // ring selector orbiter
-        static constexpr float selector_orbit_period = 5.0f;
-        static constexpr int num_segments = 10;
-        static constexpr int delta_angle = 360 / (2 * num_segments);
-        static constexpr int ring_outer_radius = OBSTACLE_RADIUS;
-        static constexpr int ring_width = 10;
-        const int offset_angle = fmodf(current_time / selector_orbit_period, 1.0f) * 360;
-        const int offset_angle2 = fmodf(-current_time / selector_orbit_period, 1.0f) * 360;
-        DrawCircleV(mouse, OBSTACLE_RADIUS, Fade(GRAY, 0.4f));
-        DrawRing(mouse, ring_outer_radius - ring_width, ring_outer_radius, 0, 360, 0, Fade(GRAY, 0.6f));
-        for (int i = 0; i < num_segments; ++i) {
-            const int start_angle = 2 * i * delta_angle + offset_angle;
-            const int start_angle2 = 2 * i * delta_angle + offset_angle2;
-            DrawRing(mouse, ring_outer_radius - ring_width / 2, ring_outer_radius, start_angle, start_angle + delta_angle, 0, LIGHTGRAY);
-            DrawRing(mouse, ring_outer_radius - ring_width, ring_outer_radius - ring_width / 2, start_angle2, start_angle2 + delta_angle, 0, LIGHTGRAY);
-        }
-
-        DrawCircleV(mouse, 10, LIGHTGRAY);
+        DrawSelector(mouse);
+        DrawCircleV(mouse, MOUSE_CENTER_RADIUS, LIGHTGRAY);
         DrawRectangle(goal.x - 25, goal.y - 25, 50, 50, solved ? GOAL_REACHED_COLOR : GOAL_NOT_REACHED_COLOR);
         DrawRectangle(0, ENVIRONMENT_HEIGHT, ENVIRONMENT_WIDTH, 200, {40, 40, 40, 255});
         for (int y = ENVIRONMENT_HEIGHT; y < SCREEN_HEIGHT; y += RIBBON_ROW_HEIGHT) {
@@ -214,9 +262,10 @@ int main() {
                 DrawRectangleLines(x, y, RIBBON_COL_WIDTH, RIBBON_ROW_HEIGHT, LIGHTGRAY);
             }
         }
+
         DrawText(std::string(solved ? "Reached Goal" : "Did Not Reach Goal").c_str(), 20, 1030, TEXT_HEIGHT, solved ? GOAL_REACHED_COLOR : GOAL_NOT_REACHED_COLOR);
         int fps = GetFPS();
-        DrawText(TextFormat("%2i FPS", fps), 520, 1030, TEXT_HEIGHT, (fps < 15) ? RED : ((fps < 30) ? GOLD : BLUE));
+        DrawText(TextFormat("%2i FPS", fps), 520, 1030, TEXT_HEIGHT, fpsColor(fps));
         DrawText((std::to_string(nodes.size()) + " nodes").c_str(), 1020, 1030, TEXT_HEIGHT, NODE_COUNT_COLOR);
         DrawText((std::to_string(samples) + " samples").c_str(), 1520, 1030, TEXT_HEIGHT, NODE_COUNT_COLOR);
 
