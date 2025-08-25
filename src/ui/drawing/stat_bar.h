d3405e233622e89ac0d738e68ae9d7fb3712fd1d
Ben Gravell <bjgravell@gmail.com>
2025-08-25 08:11:28 +0200

    use raygui in stat bar

--- src/ui/drawing/stat_bar.h
+++ src/ui/drawing/stat_bar.h
@@ -16,44 +16,52 @@ static constexpr int STAT_BAR_Y_MIN = 0;
 static constexpr int STAT_BAR_Y_MAX = STAT_BAR_Y_MIN + STAT_BAR_HEIGHT;
 static constexpr Rectangle STAT_BAR_REC = {STAT_BAR_X_MIN, STAT_BAR_Y_MIN, STAT_BAR_WIDTH, STAT_BAR_HEIGHT};
 
+// TODO move
+void GuiLabelColor(Rectangle bounds, const char* text, const Color color) {
+    Color color_original = GetColor(GuiGetStyle(LABEL, TEXT_COLOR_NORMAL));
+    GuiSetStyle(LABEL, TEXT_COLOR_NORMAL, ColorToInt(color));
+    GuiLabel(bounds, text);
+    GuiSetStyle(LABEL, TEXT_COLOR_NORMAL, ColorToInt(color_original));
+}
+
 void DrawStatBar(const Tree& tree, const Path path, const Vector2 goal, const bool goal_reached, const Obstacles& obstacles, const DurationParts duration_parts, const int fps, const Font font) {
     // Background
     DrawRectangleRec(STAT_BAR_REC, COLOR_STAT_BAR_BACKGROUND);
 
     static constexpr int X = STAT_BAR_X_MIN + TEXT_MARGIN_WIDTH;
 
-    static constexpr int ROW_0_Y = 0 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_1_Y = 1 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_2_Y = 2 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_3_Y = 3 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_4_Y = 4 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_5_Y = 5 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_6_Y = 6 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_7_Y = 7 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_8_Y = 8 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_9_Y = 9 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_10_Y = 10 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_11_Y = 11 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_12_Y = 12 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_13_Y = 13 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
-    static constexpr int ROW_14_Y = 14 * STAT_BAR_ROW_HEIGHT + (STAT_BAR_ROW_HEIGHT - TEXT_HEIGHT_STAT) / 2;
+    static constexpr int ROW_0_Y = STAT_BAR_Y_MIN + 0 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_1_Y = STAT_BAR_Y_MIN + 1 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_2_Y = STAT_BAR_Y_MIN + 2 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_3_Y = STAT_BAR_Y_MIN + 3 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_4_Y = STAT_BAR_Y_MIN + 4 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_5_Y = STAT_BAR_Y_MIN + 5 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_6_Y = STAT_BAR_Y_MIN + 6 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_7_Y = STAT_BAR_Y_MIN + 7 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_8_Y = STAT_BAR_Y_MIN + 8 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_9_Y = STAT_BAR_Y_MIN + 9 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_10_Y = STAT_BAR_Y_MIN + 10 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_11_Y = STAT_BAR_Y_MIN + 11 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_12_Y = STAT_BAR_Y_MIN + 12 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_13_Y = STAT_BAR_Y_MIN + 13 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_14_Y = STAT_BAR_Y_MIN + 14 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_15_Y = STAT_BAR_Y_MIN + 15 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
+    static constexpr int ROW_16_Y = STAT_BAR_Y_MIN + 16 * STAT_BAR_ROW_HEIGHT + STAT_BAR_ROW_HEIGHT;
 
-    // TODO center all text
+    GuiLabelColor((Rectangle){STAT_BAR_X_MIN, ROW_1_Y, STAT_BAR_WIDTH, STAT_BAR_ROW_HEIGHT}, goal_reached ? "Goal reached" : "Goal missed", computeGoalColor(goal_reached));
 
-    DrawTextEx(font, std::string(goal_reached ? "Goal reached" : "Goal missed").c_str(), {X, ROW_1_Y}, TEXT_HEIGHT_STAT, 1, computeGoalColor(goal_reached));
+    GuiLabelColor((Rectangle){STAT_BAR_X_MIN, ROW_2_Y, STAT_BAR_WIDTH, STAT_BAR_ROW_HEIGHT}, (std::to_string(std::lround(computeHeuristicCost(path.back(), goal))) + " path cost").c_str(), COLOR_STAT);
 
-    DrawTextEx(font, (std::to_string(std::lround(computeHeuristicCost(path.back(), goal))) + " path cost").c_str(), {X, ROW_2_Y}, TEXT_HEIGHT_STAT, 1, COLOR_STAT);
+    GuiLabelColor((Rectangle){STAT_BAR_X_MIN, ROW_4_Y, STAT_BAR_WIDTH, STAT_BAR_ROW_HEIGHT}, (std::to_string(tree.nodes.size()) + " nodes").c_str(), COLOR_STAT);
 
-    DrawTextEx(font, (std::to_string(tree.nodes.size()) + " nodes").c_str(), {X, ROW_4_Y}, TEXT_HEIGHT_STAT, 1, COLOR_STAT);
+    GuiLabelColor((Rectangle){STAT_BAR_X_MIN, ROW_5_Y, STAT_BAR_WIDTH, STAT_BAR_ROW_HEIGHT}, (std::to_string(obstacles.size()) + " obstacles").c_str(), COLOR_STAT);
 
-    DrawTextEx(font, (std::to_string(obstacles.size()) + " obstacles").c_str(), {X, ROW_5_Y}, TEXT_HEIGHT_STAT, 1, COLOR_STAT);
+    GuiLabelColor((Rectangle){STAT_BAR_X_MIN, ROW_7_Y, STAT_BAR_WIDTH, STAT_BAR_ROW_HEIGHT}, TextFormat("%4d ms [grow]", int(1000.0f * duration_parts.grow)), COLOR_TIMING_STAT);
+    GuiLabelColor((Rectangle){STAT_BAR_X_MIN, ROW_8_Y, STAT_BAR_WIDTH, STAT_BAR_ROW_HEIGHT}, TextFormat("%4d ms [carryover]", int(1000.0f * duration_parts.carryover)), COLOR_TIMING_STAT);
+    GuiLabelColor((Rectangle){STAT_BAR_X_MIN, ROW_9_Y, STAT_BAR_WIDTH, STAT_BAR_ROW_HEIGHT}, TextFormat("%4d ms [draw]", int(1000.0f * duration_parts.draw)), COLOR_TIMING_STAT);
+    GuiLabelColor((Rectangle){STAT_BAR_X_MIN, ROW_10_Y, STAT_BAR_WIDTH, STAT_BAR_ROW_HEIGHT}, TextFormat("%4d ms [total]", int(1000.0f * duration_parts.total)), COLOR_TIMING_STAT);
 
-    DrawTextEx(font, TextFormat("%4d ms [grow]", int(1000.0f * duration_parts.grow)), {X, ROW_7_Y}, TEXT_HEIGHT_STAT, 1, COLOR_TIMING_STAT);
-    DrawTextEx(font, TextFormat("%4d ms [carryover]", int(1000.0f * duration_parts.carryover)), {X, ROW_8_Y}, TEXT_HEIGHT_STAT, 1, COLOR_TIMING_STAT);
-    DrawTextEx(font, TextFormat("%4d ms [draw]", int(1000.0f * duration_parts.draw)), {X, ROW_9_Y}, TEXT_HEIGHT_STAT, 1, COLOR_TIMING_STAT);
-    DrawTextEx(font, TextFormat("%4d ms [total]", int(1000.0f * duration_parts.total)), {X, ROW_10_Y}, TEXT_HEIGHT_STAT, 1, COLOR_GRAY_192);
-
-    DrawTextEx(font, TextFormat("%2i FPS", fps), {X, ROW_13_Y}, TEXT_HEIGHT_STAT, 1, computeFpsColor(fps));
+    GuiLabelColor((Rectangle){STAT_BAR_X_MIN, ROW_13_Y, STAT_BAR_WIDTH, STAT_BAR_ROW_HEIGHT}, TextFormat("%2i FPS", fps), computeFpsColor(fps));
 
     // Border
     DrawRectangleLinesEx(STAT_BAR_REC, 3, COLOR_STAT_BAR_BORDER);
